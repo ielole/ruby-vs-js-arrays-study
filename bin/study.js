49ae81b98a612d15f90e2aa149f64c1bf907140b
Luyuan Xing <lxing.nyc@gmail.com>
2016-11-01 16:05:30 -0400

    new solution to updated question 4
    wording of question updated as per issue #9 in the ruby-vs-
    js-arrays repo

--- bin/study.js
+++ bin/study.js
@@ -4,16 +4,17 @@
 
 let myArray = [1, 2, 3, 4, 5];
 // Assign `20` to the element at `length+1`.
-myArray[myArray.length+1] = 20
+myArray[myArray.length+1] = 20;
 
 // Access the 3rd element from the end of an array length of 5.
-myArray[myArray.length-4]
+myArray[myArray.length-3];
 
 // Access element 9 for a length of 5 elements.
-myArray[9]
+myArray[9];
 
-// Assign `[-12, -49]` to the 5th element from the end.
-myArray[myArray.length-6] = [-12, -49]
+// Assign [-12, -49] to the range starting from 5th element from the end, for a length of 3.
+myArray.splice(myArray.length-5, 3, [-12, -29]);
+myArray = [].concat.apply([], myArray);
 
 // Access all the elements starting at index 1.
 for (let i = 1; i < myArray.length; i++) {
